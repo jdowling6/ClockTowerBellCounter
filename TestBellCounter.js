d76ab5544fba2995367973ee858395a62cb22e44
John Dowling <john_a_dowling@homedepot.com>
2017-06-26 13:16:35 -0400

    WIP

--- TestBellCounter.js
+++ TestBellCounter.js
@@ -4,29 +4,42 @@
 	function TestBellCounter() {
 
 		function testCountBells() {
-			var isPassing = true;
+			var bc = new BellCounter(),
+				isPassing = true;
 
-			//TC1 Treat same times as 24 hour passing
-			//If either time is on the hour, count the tolls for that hour
-
-			//TC1 1:00, 1:00 => 157 = (1+2+3+4+5+6+7+8+9+10+11+12+1+2+3+4+5+6+7+8+9+10+11+12+1)
-			//TC2 9:00, 9:00 => = 165 = (9+10+11+12+1+2+3+4+5+6+7+8+9+10+11+12+1+2+3+4+5+6+7+8+9)
-			//TC3 11:00, 11:00 => 167 = (11+12+1+2+3+4+5+6+7+8+9+10+11+12+1+2+3+4+5+6+7+8+9+10+11)
-			//TC4 2:00, 3:00 => 5
-			//TC5 14:00, 15:00 => 5
-			//TC6 14:23, 15:42 => 3
-			//TC7 23:00, 1:00 => 24
-
-			//var bc = new BellCounter();
-
-			//bc.countBells('11:00', '11:00');
-
+			isPassing &= (157 === bc.countBells('1:00', '1:00'));
+			isPassing &= (165 === bc.countBells('9:00', '9:00'));
+			isPassing &= (167 === bc.countBells('11:00', '11:00'));
+			isPassing &= (5 === bc.countBells('2:00', '3:00'));
+			isPassing &= (5 === bc.countBells('14:00', '15:00'));
+			isPassing &= (3 === bc.countBells('14:23', '15:42'));
+			isPassing &= (24 === bc.countBells('23:00', '1:00'));
 
 			return isPassing;
 		}
 
 		function testValidateInput() {
+			var bc = new BellCounter(),
+				isPassing = true;
 
+			isPassing &= !bc.validateInput('1', '0:00');
+			isPassing &= !bc.validateInput('0:00', '1');
+			isPassing &= !bc.validateInput('0:0:0', '00:00');
+			isPassing &= !bc.validateInput('00:00', '0:0:0');
+			isPassing &= !bc.validateInput('00:aa', '00:00');
+			isPassing &= !bc.validateInput('aa:00', '00:00');
+			isPassing &= !bc.validateInput('00:00', '00:aa');
+			isPassing &= !bc.validateInput('00:00', 'aa:00');
+			isPassing &= !bc.validateInput('-1:00', '00:00');
+			isPassing &= !bc.validateInput('24:00', '00:00');
+			isPassing &= !bc.validateInput('00:-1', '00:00');
+			isPassing &= !bc.validateInput('00:60', '00:00');
+			isPassing &= !bc.validateInput('00:00', '-1:00');
+			isPassing &= !bc.validateInput('00:00', '24:00');
+			isPassing &= !bc.validateInput('00:00', '00:-1');
+			isPassing &= !bc.validateInput('00:00', '00:60');
+
+			return isPassing;
 		}
 
 	}
